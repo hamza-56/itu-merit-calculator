ca5612122b9193e42e4f10b0040f7d328f09b676
Hamza <41573849+hamza-56@users.noreply.github.com>
2020-05-30 03:53:48 +0500

    add MS tool

--- js/aggregate.js
+++ js/aggregate.js
@@ -1,33 +1,34 @@
 function calculateBSMerit(event) {
   var part1 = parseInt(document.getElementById('part1').value);
-  var part1total = parseInt(document.getElementById('part1total').value);
+  var part1Total = parseInt(document.getElementById('part1Total').value);
   var matric = parseInt(document.getElementById('matric').value);
-  var matrictotal = parseInt(document.getElementById('matrictotal').value);
-  var test = parseInt(document.getElementById('test').value); // itu marks
-  var testtotal = parseInt(document.getElementById('testtotal').value);
+  var matricTotal = parseInt(document.getElementById('matricTotal').value);
+  var BSTest = parseInt(document.getElementById('BSTest').value);
+  var BSTestTotal = parseInt(document.getElementById('BSTestTotal').value);
 
-  if (isNaN(part1) || isNaN(part1total) || isNaN(matric) || isNaN(matrictotal) || isNaN(test) || isNaN(testtotal)) {
+  if (isNaN(part1) || isNaN(part1Total) || isNaN(matric) || isNaN(matricTotal) || isNaN(BSTest) || isNaN(BSTestTotal)) {
     console.log('something went wrong');
   } else {
     event.preventDefault();
-    var sum = (0.25 * (matric / matrictotal) + 0.25 * (part1 / part1total) + 0.5 * (test / testtotal)) * 100;
+    var sum = (0.25 * (matric / matricTotal) + 0.25 * (part1 / part1Total) + 0.5 * (BSTest / BSTestTotal)) * 100;
     $('#BSResult').html('<h3 class="mt-5 text-primary">Your Aggregate = ' + sum.toFixed(2) + '% </h3>');
   }
 }
 
 function calculateMSMerit(event) {
-  var part1 = parseInt(document.getElementById('part1').value);
-  var part1total = parseInt(document.getElementById('part1total').value);
-  var matric = parseInt(document.getElementById('matric').value);
-  var matrictotal = parseInt(document.getElementById('matrictotal').value);
-  var test = parseInt(document.getElementById('test').value); // itu marks
-  var testtotal = parseInt(document.getElementById('testtotal').value);
+  var undergrad = parseFloat(document.getElementById('undergrad').value);
+  var undergradTotal = 4.0;
+  var interview = parseFloat(document.getElementById('interview').value);
+  var interviewTotal = parseFloat(document.getElementById('interviewTotal').value);
+  var MSTest = parseInt(document.getElementById('MSTest').value);
+  var MSTestTotal = parseInt(document.getElementById('MSTestTotal').value);
 
-  if (isNaN(part1) || isNaN(part1total) || isNaN(matric) || isNaN(matrictotal) || isNaN(test) || isNaN(testtotal)) {
+  if (isNaN(undergrad) || isNaN(interview) || isNaN(interviewTotal) || isNaN(MSTest) || isNaN(MSTestTotal)) {
     console.log('something went wrong');
   } else {
     event.preventDefault();
-    var sum = (0.25 * (matric / matrictotal) + 0.25 * (part1 / part1total) + 0.5 * (test / testtotal)) * 100;
-    $('#BSResult').html('<h3 class="mt-5 text-primary">Your Aggregate = ' + sum.toFixed(2) + '% </h3>');
+    var sum =
+      (0.4 * (undergrad / undergradTotal) + 0.2 * (interview / interviewTotal) + 0.4 * (MSTest / MSTestTotal)) * 100;
+    $('#MSResult').html('<h3 class="mt-5 text-primary">Your Aggregate = ' + sum.toFixed(2) + '% </h3>');
   }
 }
